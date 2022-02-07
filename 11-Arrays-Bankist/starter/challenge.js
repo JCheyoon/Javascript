4d2bb2ba0ef05bdcfd488dd0adf2104678a78c50
Cheyoon Jung <jessietwo94@gmail.com>
2022-02-07 17:52:52 +0100

    update

--- 11-Arrays-Bankist/starter/challenge.js
+++ 11-Arrays-Bankist/starter/challenge.js
@@ -1,5 +1,5 @@
-'use strict'
-
+'use strict';
+/*
 const dogsJulia = [3, 5, 2, 12, 7]
 const dogsKate =  [4, 1, 15, 8, 3]
 
@@ -20,3 +20,80 @@ dogs.forEach(checkAge)
 
 }
 checkDogs(dogsJulia,dogsKate)
+ */
+
+/*
+//challenge2 :my code
+const dogsAge = [5, 2, 4, 1, 15, 8, 3]
+
+const calcAverageHumanAge = dogsAge.map((age) => {
+  const humanAge = age <=2 ? 2 * age : 16 + age * 4
+  return humanAge
+})
+// const calcAverageHumanAge2 = dogsAge.map((age) => age <=2 ? 2 * age : 16 + age * 4)
+
+// console.log(calcAverageHumanAge)
+const exclude18ageDogs = calcAverageHumanAge.filter(age => age >=18)
+// console.log(exclude18ageDogs)
+const averageHumanAge = exclude18ageDogs.reduce((acc,cur,) => {return acc + cur},0)/ exclude18ageDogs.length
+
+console.log(averageHumanAge)
+
+// const doSthg1 = (a, b) => (a + b)
+// const doSthg2 = (a, b) => {return a + b}
+
+//teacher code
+const calcAverageHumanAge2 = function (ages) {
+  const humanAges = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
+  const adults = humanAges.filter(age => age >= 18);
+  console.log(humanAges);
+  console.log(adults);
+
+  // const average = adults.reduce((acc, age) => acc + age, 0) / adults.length;
+
+  const average2 = adults.reduce(
+    (acc, age, i, arr) => acc + age / arr.length,
+    0
+  );
+
+  // 2 3. (2+3)/2 = 2.5 === 2/2+3/2 = 2.5
+
+  return average2;
+};
+const avg1 = calcAverageHumanAge2([5, 2, 4, 1, 15, 8, 3]);
+const avg2 = calcAverageHumanAge2([16, 6, 10, 5, 6, 1, 4]);
+console.log(avg1, avg2);
+*/
+
+//challenge3 :my code
+/*
+Rewrite the 'calcAverageHumanAge' function from Challenge #2, but this time
+as an arrow function, and using chaining!
+  Test data:
+  § Data 1: [5, 2, 4, 1, 15, 8, 3]
+§ Data 2: [16, 6, 10, 5, 6, 1, 4]
+ */
+const calcAverageHumanAge = ages => {
+  // const result = ages
+  //   .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
+  //   .filter(age => age >= 18)
+  //   .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+  //
+  // return result;
+  return ages
+    .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
+    .filter(age => age >= 18)
+    .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+};
+
+const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+console.log(avg1, avg2);
+
+// const add = (a, b) => {
+//   // const result = a + b
+//   //
+//   // return result
+//
+//   return a + b;
+// }
--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -61,30 +61,134 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-const displayMovement = function (movements){
+const displayMovement = function (movements) {
   containerMovements.innerHTML = '';
 
-movements.forEach(function (mov,i){
-  const type = mov > 0 ? 'deposit' : 'withdrawal'
-  const html = `
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+    const html = `
         <div class="movements__row">
-          <div class="movements__type movements__type--${type}">${i+1}${type}</div>
+          <div class="movements__type movements__type--${type}">${
+      i + 1
+    }${type}</div>
           <div class="movements__value">${mov}€</div>`;
-    containerMovements.insertAdjacentHTML('afterbegin',html)
-})
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+const calcDisplayBalance = function (acc) {
+  acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
+  //create balance field under account object
+  labelBalance.textContent = `${acc.balance} EUR`;
+};
+
+const calcDisplaySummary = function (acc) {
+  const incomes = acc.movements
+    .filter(mov => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `${incomes}€`;
+
+  const out = acc.movements
+    .filter(mov => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `${Math.abs(out)}€`;
+
+  const interest = acc.movements
+    .filter(mov => mov > 0)
+    .map(deposit => (deposit * acc.interestRate) / 100)
+    .filter(int => int >= 1)
+    .reduce((acc, int) => acc + int, 0);
+  labelSumInterest.textContent = `${interest}€`;
+};
+
+const createUsernames = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+};
+
+createUsernames(accounts);
+
+const updateUI = function (acc){
+  //display movement
+  displayMovement(currentAcc.movements)
+  //display balance
+  calcDisplayBalance(currentAcc)
+  //display summary
+  calcDisplaySummary(currentAcc)
 }
-displayMovement(account1.movements)
+
+//Event Handler
+let currentAcc
+
+btnLogin.addEventListener('click',function (e){
+  //prevent form from submitting ->버튼 클릭이 form으로 만들어졌는데 디폴트값이 값이 입력되면 페이지를 리로드하는거, 그래서 그걸 없에주기위해 preventDefault
+  e.preventDefault()
+
+  currentAcc = accounts.find((acc) => acc.username === inputLoginUsername.value)
+  console.log(currentAcc)
+  if(currentAcc?.pin === Number(inputLoginPin.value)){
+    //display UI & welcome message
+    labelWelcome.textContent =`Welcome back, ${currentAcc.owner.split(' ')[0]}`
+    containerApp.style.opacity = '1'
+    //clear input fields
+    inputLoginUsername.value = inputLoginPin.value = '';
+    // inputLoginPin.blur();
+    updateUI(currentAcc)
+  }
+})
+
+btnTransfer.addEventListener('click',function (e){
+  e.preventDefault();
+  const amount = Number(inputTransferAmount.value)
+  const receiverAcc = accounts.find(acc => acc.username ===inputTransferTo.value)
+  inputTransferAmount.value = inputTransferTo.value = '';
+
+  if(amount > 0 && receiverAcc &&
+    currentAcc.balance >= amount && receiverAcc.username !== currentAcc){
+    currentAcc.movements.push(-amount);
+    receiverAcc.movements.push(amount)
+
+    //update UI
+    updateUI(currentAcc)
+  }
+});
+btnLoan.addEventListener('click',function (e){
+  e.preventDefault();
+
+  const amount = Number(inputLoanAmount.value);
+
+  if(amount>0 && currentAcc.movements.some(mov => mov >= amount*0.1)){
+   //add movement
+   currentAcc.movements.push(amount)
+   //update ui
+    updateUI(currentAcc)
+  }
+  inputLoanAmount.value = ''
+});
+
+btnClose.addEventListener('click',function (e){
+  e.preventDefault()
+  if (inputCloseUsername.value === currentAcc.username && Number(inputClosePin.value) === currentAcc.pin){
+    const index = accounts.findIndex(acc => acc.username === currentAcc.username)
+    //findIndex,find can get access current index, current entire array
+    //delete account
+    accounts.splice(index, 1);
+    //hide Ui
+    containerApp.style.opacity = 0;
+  }
+  inputCloseUsername.value = inputClosePin.value='';
+
+})
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
-/*
-// const currencies = new Map([
-//   ['USD', 'United States dollar'],
-//   ['EUR', 'Euro'],
-//   ['GBP', 'Pound sterling'],
-// ]);
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+/*
 
 //Slice
 let arr =['a','b','c','d','e']
@@ -162,4 +266,119 @@ console.log(currenciesUnique)
 currenciesUnique.forEach(function (value,_,map){
   console.log(`${value} :${value}`)
 })
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
+
+//
+
 */
+//
+//
+// const eurToUsd = 1.1
+// //functional programming
+// const movementsUSD = movements.map(mov => mov * eurToUsd)
+// console.log(movements)
+// console.log(movementsUSD)
+// //--
+// const movementUSDfor = [];
+// for(const mov of movements) movementUSDfor.push(mov * eurToUsd)
+// console.log(movementUSDfor);
+// //--
+// const movementDescriptions = movements.map((mov,i)=>
+//   `Movement ${i+1}: You ${mov>0 ? 'deposited' : 'withdrew'}${Math.abs(mov)}`
+// )
+//
+// console.log(movementDescriptions);
+
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+//
+// const deposits = movements.filter(function (mov){
+//   return mov > 0;
+// })
+// console.log(movements)
+// console.log(deposits) //[200, 450, 3000, 70, 1300]
+//
+// // regular for loop
+// const depositFor = [];
+// for (const mov of movements) if (mov>0) depositFor.push(mov)
+// console.log(depositFor)
+//
+// //-
+// const withdrawals = movements.filter(function (mov2){
+//   return mov2 < 0
+// })
+// console.log(withdrawals)
+//
+// //--------------------------------reduce----------------
+// //accumulator -> SNOWBALL
+// const balance = movements.reduce(function (acc,cur,i,arr){
+//   console.log(`Iteration ${i}:${acc}`)
+//   return acc + cur
+// },0)
+// console.log(balance)
+//
+// //regular fr loop
+// let balance2 =0;
+// for (const mov of movements)balance2 +=mov
+// console.log(balance2)
+//
+// //Maximum value
+// const max = movements.reduce((acc,mov)=>{
+//   // if(acc > mov) return acc
+//   // else return mov
+//   return acc > mov ? acc :mov
+// },movements[0])
+// console.log(max)
+//-------------
+/*
+const eurToUsd = 1.1;
+const totalDepositUsd = movements
+  .filter(mov => mov > 0)
+  // .map(mov=>mov*eurToUsd)
+
+  .map((mov, i, arr) => {
+    console.log(arr);
+    return mov * eurToUsd;
+  })
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(totalDepositUsd);
+//만약 filter에서 실수했는지 아닌지 결과를 보고 싶을때 map에서 저렇게 어레이를 확인가능
+
+//find method -> filter 와 다른점은 filter은 해당값이 찾아지면 계속 넘어가 어레이를 만들고,find는 값이 찾아지면 멈춘다
+const firstWithdrawal = movements.find(mov => mov <0)
+console.log(movements)
+console.log(firstWithdrawal)
+
+console.log(accounts)
+0: {owner: 'Jonas Schmedtmann', movements: Array(8), interestRate: 1.2, pin: 1111, username: 'js'}
+1: {owner: 'Jessica Davis', movements: Array(8), interestRate: 1.5, pin: 2222, username: 'jd'}
+2: {owner: 'Steven Thomas Williams', movements: Array(8), interestRate: 0.7, pin: 3333, username: 'stw'}
+3: {owner: 'Sarah Smith', movements: Array(5), interestRate: 1, pin: 4444, username: 'ss'}
+length: 4
+const account = accounts.find(acc=>acc.owner ==='Jessica Davis')
+console.log(account)//{owner: 'Jessica Davis', movements: Array(8), interestRate: 1.5, pin: 2222, username: 'jd'}
+*/
+
+//some & every
+console.log(movements);
+
+//equality
+console.log(movements.includes(-130)) // true
+
+//some: condition
+// movements.some((mov) => mov === -130)
+const anyDeposits = movements.some((mov) => mov > 0)
+console.log(anyDeposits) //true
+
+//every ->모든 값이 해당조건과 맞냐?
+console.log(movements.every(mov => mov >0)) //false
+
+console.log('separate callback')
+//separate callback -여러개테스트할때
+const deposit = mov => mov >0
+console.log(movements.some(deposit))
+console.log(movements.every(deposit))
+console.log(movements.filter(deposit))
