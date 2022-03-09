ad04bf038f8257d80cb153f47d003d9c44e2fe02
Cheyoon Jung <jessietwo94@gmail.com>
2022-03-09 09:57:54 +0100

    update

---
+++ 18-forkify/starter/src/js/model.js
@@ -0,0 +1,50 @@
+import { async } from 'regenerator-runtime'; //polyfill  async await
+import { API_URL } from './config';
+import { getJSON } from './helper';
+
+export const state = {
+  recipe: {},
+};
+
+export const loadRecipe = async function (id) {
+  try {
+    const data = await getJSON(`${API_URL}${id}`);
+
+    let { recipe } = data.data;
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      img: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(state.recipe);
+  } catch (err) {
+    //temp error handle
+    console.error(`${err}🪄`);
+    throw err;
+  }
+};
+
+export const loadSearchResults = async function (query) {
+  try {
+    const data = await getJSON(`${API_URL}?search=${query}`);
+    console.log(data);
+
+    data.data.recipes.map(rec => {
+      return {
+        id: rec.id,
+        title: rec.title,
+        publisher: rec.publisher,
+        sourceUrl: rec.source_url,
+        img: rec.image_url,
+      };
+    });
+  } catch (err) {
+    throw err;
+  }
+};
+loadSearchResults('pizza');
